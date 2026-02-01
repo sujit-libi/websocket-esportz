b03a3a299f867ea3b4dceb2f15dda5c4fc3cdada
Sujit Kumar Libi <sujit_libi@hotmail.com>
2026-02-01 15:39:21 +0545

    Implemented Code rabbit suggested fix

--- src/ws/server.js
+++ src/ws/server.js
@@ -22,10 +22,24 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on('connection', (socket) => {
+    socket.isAlive = true;
+    socket.on('pong', () => {
+      socket.isAlive = true;
+    });
     sendJson(socket, { type: 'welcome' });
 
     socket.on('error', console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 3000);
+
+  wss.on('close', () => clearInterval(interval));
 
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: 'match_created', data: match });
